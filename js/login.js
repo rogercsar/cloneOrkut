18a100bce12d9cde428cfb15248da767eeb56864
rogercsar <rogercsardev@gmail.com>
2023-06-29 14:55:04 -0400

    first commit

--- js/login.js
+++ js/login.js
@@ -3,9 +3,10 @@ function chamaLogin(){
     let inputPass = document.querySelector('#field__pass');
     let user = inputUser.value;
     let pass = inputPass.value;
+    let linkPag = document.querySelector('#linkPag');
 
     if(user && pass == "Admin"){
-        
+       linkPag.setAttribute('href', './profile.html');
     } 
     else {
         alert('Insira um usuário ou senha válido');
